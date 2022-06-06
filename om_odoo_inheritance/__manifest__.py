6b6d9c39f8d642ec21b667a5e3e8bdcb7b313e72
HanZawNyein <hanzawnyineonline@gmail.com>
2022-06-06 08:09:05 +0630

    Inherit Model

---
+++ om_odoo_inheritance/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Odoo Inheritance',
+    'version': '1.0.0',
+    'category': 'Odoo',
+    'author': 'Han Zaw Nyine',
+    'sequence': -100,
+    'summary': 'Odoo Inheritance',
+    'description': """
+    Odoo Inheritance System
+    """,
+    'depends': ['sale'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}

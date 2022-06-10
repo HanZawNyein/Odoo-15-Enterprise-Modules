31659471d45e5207cf5cd7b860c0085ea03c98c2
HanZawNyein <hanzawnyineonline@gmail.com>
2022-06-10 08:11:35 +0630

    Apply Domain For Fields

--- om_hospital/wizard/cancel_appointment.py
+++ om_hospital/wizard/cancel_appointment.py
@@ -1,7 +1,8 @@
 import datetime
 
-from odoo import api, fields, models,_
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+
 
 class CancelAppointmentWizard(models.TransientModel):
     _name = 'cancel.appointment.wizard'
@@ -14,11 +15,12 @@ class CancelAppointmentWizard(models.TransientModel):
         res['date_cancel'] = datetime.date.today()
         return res
 
-    appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointment',
+                                     domain=['|', ('state', '=', 'draft'), ('priority', 'in', ('0', '1'))])
     reason = fields.Text(string="Reason")
     date_cancel = fields.Date(string="Cancellation Date")
 
     def action_cancel(self):
         if self.appointment_id.booking_date == fields.Date.today():
-            raise ValidationError(_("test"))
+            raise ValidationError(_("Sorry, cancellation is not allowed on the same day of booking !"))
         return

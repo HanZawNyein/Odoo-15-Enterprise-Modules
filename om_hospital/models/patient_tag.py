90dfbd91ae4dec8ebb78eff0a04941a280800643
HanZawNyein <hanzawnyineonline@gmail.com>
2022-06-22 11:44:22 +0630

    Copy Function

--- om_hospital/models/patient_tag.py
+++ om_hospital/models/patient_tag.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models,_
 
 
 class PatientTag(models.Model):
@@ -6,7 +6,7 @@ class PatientTag(models.Model):
     _description = 'Patient Tag'
 
     name = fields.Char(string='Name', required=True)
-    active = fields.Boolean(string='Active', default=True)
+    active = fields.Boolean(string='Active', default=True,copy=False)
     color = fields.Integer(string='Color')
     color_2 = fields.Char(string='Color 2')
     sequence = fields.Integer(string="Sequence")
@@ -15,3 +15,14 @@ class PatientTag(models.Model):
         ('unique_tag_name', 'unique(name,active)', 'Name must be unique.'),
         ('check_sequence', 'check(sequence>0)', 'Sequence must be non zero positive number.')
     ]
+
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+
+        if not default.get("name"):
+            default["name"] = _("%s (copy)", self.name)
+
+        default['sequence'] =10
+        return super(PatientTag, self).copy(default)

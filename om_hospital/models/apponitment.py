02b5c0c906b523d76ea4778c1287786568ed3b44
HanZawNyein <hanzawnyineonline@gmail.com>
2022-06-29 09:18:04 +0630

    Monetary Field And Widget Monetary In Odoo || Odoo Currency Fields

--- om_hospital/models/apponitment.py
+++ om_hospital/models/apponitment.py
@@ -30,6 +30,8 @@ class HospitalAppointment(models.Model):
     hide_sales_price = fields.Boolean(string='Hide Sale Price')
     progress = fields.Integer(string="Progress", compute="_compute_progress")
     duration = fields.Float(string="Duration")
+    company_id = fields.Many2one('res.company',string='Company',default=lambda self:self.env.company)
+    currency_id = fields.Many2one('res.currency', related='company_id.currency_id')
 
     @api.depends('state')
     def _compute_progress(self):
@@ -91,3 +93,11 @@ class AppointmentPharmacyLines(models.Model):
     price_unit = fields.Float(related="product_id.list_price")
     qty = fields.Integer(string="Quantity", default=1)
     appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
+
+    currency_id = fields.Many2one('res.currency', related='appointment_id.currency_id')
+    price_subtotal = fields.Monetary(string='Subtotal', compute='_compute_price_subtotal')
+
+    @api.depends('price_unit', 'qty')
+    def _compute_price_subtotal(self):
+        for rec in self:
+            rec.price_subtotal = rec.price_unit * rec.qty
